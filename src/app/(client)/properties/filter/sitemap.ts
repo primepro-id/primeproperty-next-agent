766c96317141cea8acf9f4061d801fa6bc244ac3
Stanley Winata <winatastanley355@gmail.com>
2026-03-10 11:03:37 +0700

    Fix: sitemap

--- src/app/(client)/properties/filter/sitemap.ts
+++ src/app/(client)/properties/filter/sitemap.ts
@@ -1,6 +1,5 @@
 import { findProperties } from "@/lib/api/properties/find-properties";
 import { findPropertiesSitePaths } from "@/lib/api/properties/find-properties-site-paths";
-import { PurchaseStatus } from "@/lib/enums/purchase-status";
 import { env } from "@/lib/env";
 import { MetadataRoute } from "next";
 
@@ -18,192 +17,13 @@ const generatePropertySitePathSitemaps = async () => {
   return [];
 };
 
-const cleanUrl = (url: string) =>
-  String(url).replaceAll("&", "&amp;").replaceAll(" ", "+");
-
 async function generatePropertyFiltersSitemaps() {
   // Google's limit is 50,000 URLs per sitemap
-  const filters = [
-    {
-      key: "purchase_status",
-      value: PurchaseStatus.ForSale,
-    },
-    {
-      key: "purchase_status",
-      value: PurchaseStatus.ForRent,
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-    },
-    {
-      key: "buiding_type",
-      value: "tanah",
-    },
-    // province
-    {
-      key: "province",
-      value: "jakarta",
-    },
-    {
-      key: "province",
-      value: "jawa barat",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "province",
-      secondValue: "jakarta",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "province",
-      secondValue: "jawa barat",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "province",
-      secondValue: "jakarta",
-    },
-    // regencies
-    {
-      key: "regency",
-      value: "jakarta pusat",
-    },
-    {
-      key: "regency",
-      value: "jakarta selatan",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "regency",
-      secondValue: "jakarta pusat",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "regency",
-      secondValue: "jakarta selatan",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "regency",
-      secondValue: "depok",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "regency",
-      secondValue: "jakarta pusat",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "regency",
-      secondValue: "jakarta selatan",
-    },
-    // street
-    {
-      key: "street",
-      value: "kuningan",
-    },
-    {
-      key: "street",
-      value: "kemang",
-    },
-    {
-      key: "street",
-      value: "pondok indah",
-    },
-    {
-      key: "street",
-      value: "kebayoran baru",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "street",
-      secondValue: "kuningan",
-    },
-    // street home
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "street",
-      secondValue: "kemang",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "street",
-      secondValue: "pondok indah",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "street",
-      secondValue: "kebayoran baru",
-    },
-    {
-      key: "buiding_type",
-      value: "rumah",
-      secondKey: "street",
-      secondValue: "lebak bulus",
-    },
-    // street apartment
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "street",
-      secondValue: "kuningan",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "street",
-      secondValue: "lebak bulus",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "street",
-      secondValue: "kemang",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "street",
-      secondValue: "pondok indah",
-    },
-    {
-      key: "buiding_type",
-      value: "apartemen",
-      secondKey: "street",
-      secondValue: "kebayoran baru",
-    },
-  ];
+
   const basePropertySitemap = await findProperties({
     page: String(1),
     limit: String(30),
   });
-  const promises = await Promise.allSettled([
-    ...filters.map(async ({ key, value }) => {
-      const response = await findProperties({
-        [key]: value,
-        page: String(1),
-        limit: String(30),
-      });
-      return response;
-    }),
-  ]);
 
   const sitemaps = [];
 
@@ -213,36 +33,6 @@ async function generatePropertyFiltersSitemaps() {
         url: env.NEXT_PUBLIC_HOST_URL + `/properties?page=${i + 1}`,
         date: new Date(),
       });
-    }
-  }
-
-  for (let i = 0; i < filters.length; i++) {
-    const filterPromise = promises[i];
-    if (filterPromise.status === "fulfilled") {
-      if (
-        filterPromise.value.data &&
-        filterPromise.value?.data?.total_pages > 0
-      ) {
-        for (let j = 1; j <= filterPromise.value.data.total_pages; j++) {
-          const url = new URL(env.NEXT_PUBLIC_HOST_URL + `/properties`);
-          const filterValue = filters[i].value;
-          url.searchParams.set(filters[i].key, filterValue);
-
-          if (filters[i].secondKey && filters[i].secondValue) {
-            const secondFilterValue = String(filters[i].secondValue);
-            url.searchParams.set(
-              String(filters[i]?.secondKey),
-              secondFilterValue,
-            );
-          }
-
-          url.searchParams.set("page", String(j));
-          sitemaps.push({
-            url: cleanUrl(url.toString()),
-            lastModified: new Date(),
-          });
-        }
-      }
     }
   }
 
