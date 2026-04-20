99a6d30ab92b2513e4dc384be373da0dd35d24c7
Stanley Winata <winatastanley355@gmail.com>
2026-04-20 12:59:39 +0700

    fix: Design

--- src/app/(client)/properties/_components/specifications.tsx
+++ src/app/(client)/properties/_components/specifications.tsx
@@ -7,23 +7,23 @@ type PropertyCardProps = {
 
 export const Specifications = ({ propertyWithAgent }: PropertyCardProps) => {
   return (
-    <div className="flex items-center gap-2 bg-primary/10 text-primary-foreground px-1 rounded w-fit">
+    <div className="flex items-center gap-2 bg-primary/10 text-muted-foreground px-1 rounded w-fit">
       {propertyWithAgent[0].specifications.bedrooms > 0 && (
-        <span className="flex items-center gap-1">
-          <LuBedDouble className="text-muted-foreground " />
+        <span className="flex items-center gap-1 ">
+          <LuBedDouble />
           <div>{propertyWithAgent[0].specifications.bedrooms}</div>
         </span>
       )}
       {propertyWithAgent[0].specifications.bathrooms > 0 && (
         <span className="flex items-center gap-1">
-          <LuBath className="text-muted-foreground" />
+          <LuBath />
           <div>{propertyWithAgent[0].specifications.bathrooms}</div>
         </span>
       )}
       {(propertyWithAgent[0].specifications.garage > 0 ||
         propertyWithAgent[0].specifications.carport > 0) && (
         <span className="flex items-center gap-1">
-          <LuCar className="text-muted-foreground" />
+          <LuCar />
           <div>
             {propertyWithAgent[0].specifications.garage +
               propertyWithAgent[0].specifications.carport}
@@ -32,13 +32,13 @@ export const Specifications = ({ propertyWithAgent }: PropertyCardProps) => {
       )}
       {propertyWithAgent[0].measurements.land_area > 0 && (
         <span className="flex items-center gap-1">
-          <div className="text-muted-foreground">LT:</div>
+          <div>LT:</div>
           <div>{propertyWithAgent[0].measurements.land_area} m²</div>
         </span>
       )}
       {propertyWithAgent[0].measurements.building_area > 0 && (
         <span className="flex items-center gap-1">
-          <div className="text-muted-foreground">LB:</div>
+          <div>LB:</div>
           <div>{propertyWithAgent[0].measurements.building_area} m²</div>
         </span>
       )}
