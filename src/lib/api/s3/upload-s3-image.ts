f97fbb63fd3911da8608977e76152f9efd885170
Stanley Winata <winatastanley355@gmail.com>
2026-06-11 15:35:32 +0700

    feat: Update property images

---
+++ src/lib/api/s3/upload-s3-image.ts
@@ -0,0 +1,28 @@
+import { JsonApiResponse } from "../fetch-api";
+import { PropertyImage } from "@/lib/enums/property-image";
+import { getAccessToken } from "@/lib/cookie/get-access-token";
+import { env } from "@/lib/env";
+export const uploadS3Images = async (
+  files: File[] | null,
+): Promise<JsonApiResponse<PropertyImage[]> | undefined> => {
+  if (!files) {
+    return undefined;
+  }
+  const formData = new FormData();
+  files.forEach((file) => {
+    formData.append("images", file);
+  });
+
+  try {
+    const res = await fetch(env.NEXT_PUBLIC_API_URL + `/s3/images`, {
+      headers: {
+        "x-access-token": await getAccessToken(),
+      },
+      method: "POST",
+      body: formData,
+    });
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
