741e9899cb1e003d0a7f0d34e8b94fe8f519ff5d
Stanley Winata <winatastanley355@gmail.com>
2026-06-13 14:43:49 +0700

    feat: Update config

--- src/lib/api/s3/upload-s3-image.ts
+++ src/lib/api/s3/upload-s3-image.ts
@@ -1,12 +1,11 @@
-"use client";
 import { JsonApiResponse } from "../fetch-api";
 import { PropertyImage } from "@/lib/enums/property-image";
 import { getAccessToken } from "@/lib/cookie/get-access-token";
 import { env } from "@/lib/env";
+
 export const uploadS3Images = async (
   files: File[] | null,
 ): Promise<JsonApiResponse<PropertyImage[]> | undefined> => {
-  console.log("[uploadS3Images]");
   if (!files) {
     return undefined;
   }
@@ -23,7 +22,6 @@ export const uploadS3Images = async (
       method: "POST",
       body: formData,
     });
-    console.log("[uploadS3Images] SUCCESS:");
     return await res.json();
   } catch (error) {
     console.error("[uploadS3Images] ERROR", error);
