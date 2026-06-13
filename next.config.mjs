5c4507708b1ab0b6965f37f27c77bfdb3e64b219
Stanley Winata <winatastanley355@gmail.com>
2026-06-13 14:46:47 +0700

    feat: standlone

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone",
+  output: "standalone",
   images: {
     remotePatterns: [
       {
